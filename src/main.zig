9da4c00618c39d566132d95022fdba841a1962e6
megaNour <alkotobnour@gmail.com>
2026-01-01 13:29:12 +0100

    dump pre-run

--- src/main.zig
+++ src/main.zig
@@ -9,11 +9,13 @@ pub fn main() !void {
         @embedFile("resources/simple_schema.json"),
         .{},
     );
-    std.debug.print("schema is:\n{s}\n", .{parsed.value.object.get("properties").?.object.get("age").?.object.get("type").?.string});
+    std.debug.print("pub const MySchema = struct {{\n", .{});
     var iterator = parsed.value.object.get("properties").?.object.iterator();
     while (iterator.next()) |entry| {
         walkEntry(&entry);
+        std.debug.print(",\n", .{});
     }
+    std.debug.print("}}\n", .{});
 }
 
 pub fn walkEntry(entry: *const ObjectMap.Entry) void {
@@ -21,12 +23,12 @@ pub fn walkEntry(entry: *const ObjectMap.Entry) void {
         .object => |value| {
             var iterator = value.iterator();
             while (iterator.next()) |sub_entry| {
-                std.debug.print("entry: key: \"{s}\"\n", .{sub_entry.key_ptr.*});
+                std.debug.print("\"{s}\"", .{sub_entry.key_ptr.*});
                 walkEntry(&sub_entry);
             }
         },
         .string => |value| {
-            std.debug.print("value: \"{s}\"\n", .{value});
+            std.debug.print("\"{s}\"", .{value});
         },
         else => {
             std.debug.panic("cannot handle type: {}", .{entry.value_ptr.*});
