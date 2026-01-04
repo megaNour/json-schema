2c40deac24184113a51033aca44292713f023c75
megaNour <alkotobnour@gmail.com>
2026-01-04 23:08:05 +0100

    modularize and repackage

---
+++ src/generator.zig
@@ -0,0 +1,60 @@
+const std = @import("std");
+const File = std.fs.File;
+
+pub fn generateZigCode(in: File, out: File) !void {
+    // Read and parse json
+    const stat = try in.stat();
+    const input_file_buffer = try std.heap.page_allocator.alloc(u8, stat.size);
+    defer std.heap.page_allocator.free(input_file_buffer);
+    _ = try in.readAll(input_file_buffer);
+
+    const parsed = try std.json.parseFromSlice(
+        std.json.Value,
+        std.heap.page_allocator, // will be wrapped in an arena anyway
+        input_file_buffer,
+        .{},
+    );
+    defer parsed.deinit();
+
+    // Generate zig code
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    var zig_source_buffer = try std.ArrayList(u8).initCapacity(allocator, 128_000);
+    defer zig_source_buffer.deinit(allocator);
+    try zig_source_buffer.appendSlice(allocator, "pub const Schema = struct {");
+    var iterator = parsed.value.object.get("properties").?.object.iterator();
+    while (iterator.next()) |entry| {
+        try walkEntry(&entry, allocator, &zig_source_buffer);
+        try zig_source_buffer.appendSlice(allocator, ",");
+    }
+    try zig_source_buffer.appendSlice(allocator, "};");
+    try zig_source_buffer.append(allocator, 0);
+    const tree_input: []const u8 = zig_source_buffer.items;
+    var tree = try std.zig.Ast.parse(allocator, tree_input[0 .. tree_input.len - 1 :0], .zig);
+    defer tree.deinit(allocator);
+    const rendered_buffer = try tree.renderAlloc(allocator);
+    defer allocator.free(rendered_buffer);
+    try out.writeAll(rendered_buffer);
+}
+
+fn walkEntry(entry: *const std.json.ObjectMap.Entry, allocator: std.mem.Allocator, buffer: *std.ArrayList(u8)) !void {
+    switch (entry.value_ptr.*) {
+        .object => |value| {
+            try buffer.appendSlice(allocator, entry.key_ptr.*);
+            try buffer.append(allocator, ':');
+            var iterator = value.iterator();
+            while (iterator.next()) |sub_entry| {
+                try walkEntry(&sub_entry, allocator, buffer);
+            }
+        },
+        .string => |value| {
+            if (std.mem.eql(u8, value, "string")) {
+                try buffer.appendSlice(allocator, "[]const u8");
+            } else if (std.mem.eql(u8, value, "integer")) try buffer.appendSlice(allocator, "u8");
+        },
+        else => {
+            return error.UnsupportedToken;
+        },
+    }
+}
--- src/main.zig
+++ src/main.zig
@@ -1,89 +1,36 @@
 const std = @import("std");
 const ObjectMap = std.json.ObjectMap;
 const ArgIterator = std.process.ArgIterator;
+const File = std.fs.File;
 
-const json_schema = @import("json_schema");
+const generateZigCode = @import("json_schema").generateZigCode;
 const jump = @import("jump");
 
 const ArgError = error{ InputFileMissing, OutputFileMissing };
 const ParsingError = error{UnsupportedToken};
 
 pub fn main() !void {
-    // A bit of setup
     var stderr_buffer: [64]u8 = undefined;
-    var stderr_writer = std.fs.File.stderr().writer(&stderr_buffer);
+    var stderr_writer = File.stderr().writer(&stderr_buffer);
     const stderr = &stderr_writer.interface;
 
     const args_iterator = try std.process.argsWithAllocator(std.heap.page_allocator);
     var pos_jumper = jump.OverPosLean(ArgIterator).init(args_iterator);
     _ = pos_jumper.next();
 
-    // Args parsing
-    const input_file = try std.fs.cwd().openFile(pos_jumper.next() orelse {
+    const in = try std.fs.cwd().openFile(pos_jumper.next() orelse {
         try stderr.writeAll("no input file provided. TODO: print help :-)\n");
         try stderr.flush();
         std.process.exit(1);
     }, .{ .mode = .read_only });
-    defer input_file.close();
+    defer in.close();
 
-    const output_file = try std.fs.cwd().createFile(pos_jumper.next() orelse {
+    const out = try std.fs.cwd().createFile(pos_jumper.next() orelse {
         try stderr.writeAll("no output file provided. TODO: print help :-)\n");
         try stderr.flush();
         std.process.exit(1);
     }, .{});
-    defer output_file.close();
+    defer out.close();
 
-    // Read and parse json
-    const stat = try input_file.stat();
-    const input_file_buffer = try std.heap.page_allocator.alloc(u8, stat.size);
-    defer std.heap.page_allocator.free(input_file_buffer);
-    _ = try input_file.readAll(input_file_buffer);
-
-    const parsed = try std.json.parseFromSlice(
-        std.json.Value,
-        std.heap.page_allocator, // will be wrapped in an arena anyway
-        input_file_buffer,
-        .{},
-    );
-    defer parsed.deinit();
-
-    // Generate zig code
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-    var zig_source_buffer = try std.ArrayList(u8).initCapacity(allocator, 128_000);
-    try zig_source_buffer.appendSlice(allocator, "pub const Schema = struct {");
-    var iterator = parsed.value.object.get("properties").?.object.iterator();
-    while (iterator.next()) |entry| {
-        try walkEntry(&entry, allocator, &zig_source_buffer);
-        try zig_source_buffer.appendSlice(allocator, ",");
-    }
-    try zig_source_buffer.appendSlice(allocator, "};");
-    try zig_source_buffer.append(allocator, 0);
-    const tree_input: []const u8 = zig_source_buffer.items;
-    var tree = try std.zig.Ast.parse(allocator, tree_input[0 .. tree_input.len - 1 :0], .zig);
-    defer tree.deinit(allocator);
-    const rendered_buffer = try tree.renderAlloc(allocator);
-    try output_file.writeAll(rendered_buffer);
-}
-
-pub fn walkEntry(entry: *const ObjectMap.Entry, allocator: std.mem.Allocator, buffer: *std.ArrayList(u8)) !void {
-    switch (entry.value_ptr.*) {
-        .object => |value| {
-            try buffer.appendSlice(allocator, entry.key_ptr.*);
-            try buffer.append(allocator, ':');
-            var iterator = value.iterator();
-            while (iterator.next()) |sub_entry| {
-                try walkEntry(&sub_entry, allocator, buffer);
-            }
-        },
-        .string => |value| {
-            if (std.mem.eql(u8, value, "string")) {
-                try buffer.appendSlice(allocator, "[]const u8");
-            } else if (std.mem.eql(u8, value, "integer")) try buffer.appendSlice(allocator, "u8");
-        },
-        else => {
-            return error.UnsupportedToken;
-        },
-    }
+    try generateZigCode(in, out);
 }
